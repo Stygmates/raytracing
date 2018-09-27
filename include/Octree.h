2e6e3582d63ef6b365ce72c057a597e99daae020
stygmates <styg.tan.dat@gmail.com>
2018-09-27 11:48:38 +0200

    Add octree center and box size

--- include/Octree.h
+++ include/Octree.h
@@ -11,17 +11,23 @@ class Octree
 		Octree *_dad;
 		Octree *_sons[8];
 		std::vector<Triangle> _triangles;
+		Point _center;
+		int _size;
 
 	public:
 		Octree();
+		Octree(Point center, int size);
 		~Octree();
 
 		Octree *get_dad();
 		Octree *get_son(int id);
+		Point get_center();
+		int get_size();
 
 		void set_dad(Octree *dad);
 		void set_son(Octree *son, int id);
 
+		bool triangle_bounding_box_intersects(Triangle triangle);
 		void add_triangle(Triangle triangle);
 };
 
