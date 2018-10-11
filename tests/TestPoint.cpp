156954405fa380d1efd7c18b2927ec9b7e20cc1c
stygmates <styg.tan.dat@gmail.com>
2018-10-11 21:25:55 +0200

    Add one more test example

--- tests/TestPoint.cpp
+++ tests/TestPoint.cpp
@@ -8,3 +8,11 @@ TEST(PointEmptyConstructor, PointEmptyConstructorTest)
     ASSERT_EQ(p.get_y(), 0);
     ASSERT_EQ(p.get_z(), 0);
 }
+
+TEST(PointConstructor, PointConstructorTest)
+{
+    Point p0(-4,-1,-5);
+    ASSERT_EQ(p0.get_x(), -4);
+    ASSERT_EQ(p0.get_y(), -1);
+    ASSERT_EQ(p0.get_z(), -5);
+}
