0b4bfc4cc0039dcc3124fa9ebdafe5c486b9ff82
stygmates <styg.tan.dat@gmail.com>
2018-10-14 17:13:28 +0200

    Revert "Merge branch 'intersect'"
    This reverts commit fe8c86b3e23b37127027d57baa1480b3fd265c0a, reversing
    changes made to 9a69be3dd3e53ea6eaa97fbea036453f726b2907.

--- include/Triangle.h
+++ include/Triangle.h
@@ -12,20 +12,15 @@ class Triangle
 		Point get_p1();
 		Point get_p2();
 		Point get_p3();
-		Vector get_normal();
-		
+
 		void set_p1(Point p1);
 		void set_p2(Point p2);
 		void set_p3(Point p3);
 		
-		Vector compute_normal();
-		double compute_D();
-		double compute_t(Vector source,Ray r);
 		Point ray_intersect(Ray r);
 		Point get_min_bounding_box();
 		Point get_max_bounding_box();
 
 	private:
-		Point _p1, _p2, _p3;
-		Vector _normal;
+		Point _p1, _p2, _p3, _normal;
 };
