35c14a4b00be81e611139a919afea8428ab34c5e
stygmates <styg.tan.dat@gmail.com>
2018-12-09 13:35:55 +0100

    Add test for the grid

---
+++ tests/TestGrid.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "../include/Grid.h"
+
+TEST(Grid, GridCreationTest)
+{
+    Point min_grid(0.f, 0.f, 0.f);
+    Point max_grid(10.f, 8.f, 6.f);
+    int step_x = 2;
+    int step_y = 1;
+    int step_z = 3;
+
+    Grid g(min_grid, max_grid, step_x, step_y, step_z);
+    ASSERT_EQ(g.get_slots().size(), 2);
+    ASSERT_EQ(g.get_slots()[0].size(), 8);
+    ASSERT_EQ(g.get_slots()[0][0].size(), 5);
+}
