480c585028c29be66cc3becd51aa8e0e0a8d80cf
FELLAH CHOUAIB <chouaib.fellah@etu.unistra.fr>
2018-12-09 03:11:43 +0100

    change ordre of function in class DDA and change name of function from find_slot to find_first_slot in .cpp

--- src/DigitalDifferentialAnalyser.cpp
+++ src/DigitalDifferentialAnalyser.cpp
@@ -1,26 +1,65 @@
 #include <DigitalDifferentialAnalyser.h>
 #include <cmath>
 #include <vector>
+#include <QDebug>
 
 using namespace std;
 
+vector<Slot*> DDA::Slots_visited(Ray r, Grid grid){
+
+
+    cout << "ray direction unit " << r << "lenght = " <<pow(r.get_direction().get_x(),2.0)+pow(r.get_direction().get_y(),2.0)+pow(r.get_direction().get_z(),2.0)<< endl;
+    DDA scan;
+    cout << "Finding visited grids" << endl;
+    vector<Slot> path = scan.find_visited_grids(r, grid);
+    cout << path.size() << endl;
+    cout << "Scanning done" << endl;
+    vector<Slot*> ptr_slot;
+    for (auto p: path)
+    {
+        cout << "Looking for " << p.get_min_slot() << endl;
+        ptr_slot.push_back( grid.get_slot( p.get_min_slot() ) );
+        cout << "Found slot " << endl;
+    }
+    cout << "Return pointers" << endl;
+    return ptr_slot;
+}
+
+
+
 vector<Slot> DDA::find_visited_grids(Ray ray_normalized, Grid grid)
 {
-    ray_normalized.set_direction(Vector( (ray_normalized.get_direction().get_x()-ray_normalized.get_source().get_x())*grid.get_step_x()+ray_normalized.get_source().get_x(),
-                                         (ray_normalized.get_direction().get_y()-ray_normalized.get_source().get_y())*grid.get_step_y()+ray_normalized.get_source().get_y(),
-                                         (ray_normalized.get_direction().get_z()-ray_normalized.get_source().get_z())*grid.get_step_z()+ray_normalized.get_source().get_z()));
+    ray_normalized.translate_ray_to_screen();
+    cout << "ray direction translate " << ray_normalized << endl;
+
+    ray_normalized.ray_unit();
+    cout << "ray direction translate " << ray_normalized << endl;
+
+    cout << "grid step " << grid.get_step_x() <<endl;
+    cout << "grid step " << grid.get_step_y() <<endl;
+    cout << "grid step " << grid.get_step_z() <<endl;
+
+
+    ray_normalized.set_direction(Vector( (ray_normalized.get_direction().get_x()-ray_normalized.get_source().get_x())*2.0/*grid.get_step_x()*/+ray_normalized.get_source().get_x(),
+                                         (ray_normalized.get_direction().get_y()-ray_normalized.get_source().get_y())*2.0/*grid.get_step_y()*/+ray_normalized.get_source().get_y(),
+                                         (ray_normalized.get_direction().get_z()-ray_normalized.get_source().get_z())*2.0/*grid.get_step_z()*/+ray_normalized.get_source().get_z()));
+
 
     Vector deltaT( (grid.get_step_x() - ray_normalized.get_source().get_x()) / ray_normalized.get_direction().get_x(),
                    (grid.get_step_y() - ray_normalized.get_source().get_y()) / ray_normalized.get_direction().get_y(),
                    (grid.get_step_z() - ray_normalized.get_source().get_z()) / ray_normalized.get_direction().get_z());
 
+    cout << "ray direction translate " << ray_normalized << endl;
 
+    cout << "step " << grid.get_step_x() << endl;
+    cout << "step " << grid.get_step_y() << endl;
+    cout << "step " << grid.get_step_z() << endl;
 
     float t_x = deltaT.get_x();
     float t_y = deltaT.get_y();
     float t_z = deltaT.get_z();
 
-    Slot current_slot = find_slot(grid, ray_normalized);
+    Slot current_slot = find_first_slot(grid, ray_normalized);
 
     vector<Slot> path;
     path.push_back(current_slot);   //first slot
@@ -67,7 +106,7 @@ return path;
 }
 
 
-Slot DDA::find_slot(Grid grid, Ray ray_normalized)
+Slot DDA::find_first_slot(Grid grid, Ray ray_normalized)
 {
     Point current_slot, min_slot;
     current_slot = ray_normalized.get_source() - grid.get_min_grid();
@@ -87,12 +126,3 @@ Slot DDA::find_slot(Grid grid, Ray ray_normalized)
     return slot;
 }
 
-vector<Slot*> DDA::Slots_visited(Ray r, Grid grid){
-    r.set_direction(Vector(r.get_direction().unit()));
-    DDA scan;
-    vector<Slot> path = scan.find_visited_grids(r, grid);
-    vector<Slot*> ptr_slot;
-    for (auto p: path)
-                ptr_slot.push_back( grid.get_slot( p.get_min_slot() ) );
-    return ptr_slot;
-}
