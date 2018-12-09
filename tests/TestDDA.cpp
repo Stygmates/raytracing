df98a20d67ae08fdfb297bd1b118ecf82bb00fb9
stygmates <styg.tan.dat@gmail.com>
2018-12-09 14:43:11 +0100

    Fixed tests

--- tests/TestDDA.cpp
+++ tests/TestDDA.cpp
@@ -5,9 +5,9 @@ TEST(DDA, DigitalDifferentialAnalyserTest)
 {
     Point min_grid(-150, -150, -150);
     Point max_grid(150, 150, 150);
-    int x = 15;
-    int y = 15;
-    int z = 15;
+    int x = 20;
+    int y = 20;
+    int z = 20;
 
     Grid grid(min_grid, max_grid, x, y, z);
 
@@ -15,11 +15,9 @@ TEST(DDA, DigitalDifferentialAnalyserTest)
 
     Vector direction(pis.get_x() + 0.5, pis.get_y() + 0.5, pis.get_z());
     Ray r(Point(0, 0, 0), direction);
-    r.set_direction(Vector(r.get_direction().unit() + direction));  //translate direction of normalized ray
-    Ray r_normalized(Point(pis.get_x() + 0.5, pis.get_y() + 0.5, pis.get_z()), r.get_direction());
 
     DDA scan;
-    vector<Slot> path = scan.find_visited_grids(r_normalized, grid);
+    vector<Slot> path = scan.find_visited_grids(r, grid);
 
     ASSERT_EQ(path[0].get_min_slot().get_x(), -10);
     ASSERT_EQ(path[0].get_min_slot().get_y(), 90);
