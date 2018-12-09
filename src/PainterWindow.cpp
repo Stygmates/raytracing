7174cba32cc28219a6f9988b52bc057e5a926ccc
stygmates <styg.tan.dat@gmail.com>
2018-12-09 09:28:46 +0100

    Does not draw image if it's not computed beforehand

--- src/PainterWindow.cpp
+++ src/PainterWindow.cpp
@@ -1,19 +1,23 @@
 #include "PainterWindow.h"
 #include <QDebug>
+#include <cassert>
 
 using namespace std;
 
 void PainterWindow::paintEvent(QPaintEvent*) {
     QPainter painter(this);
-    for(unsigned int j = this->image.size() - 1; j>= 0; j--)
+    if(this->image.size() != 0)
     {
-        for(unsigned int i = 0; i < this->image[j].size(); i++)
+        this->resize(this->image[0].size(), this->image.size());
+        for(unsigned int j = this->image.size() - 1; j>= 0; j--)
         {
-            Point pixel = this->image[j][i];
-            const QColor color(pixel.get_x(), pixel.get_y(), pixel.get_z());
-            painter.setPen(color);
-            painter.drawPoint(i, j);
+            for(unsigned int i = 0; i < this->image[j].size(); i++)
+            {
+                Point pixel = this->image[j][i];
+                const QColor color(pixel.get_x(), pixel.get_y(), pixel.get_z());
+                painter.setPen(color);
+                painter.drawPoint(i, j);
+            }
         }
     }
-    qDebug() << "Painting done" << endl;
 }
