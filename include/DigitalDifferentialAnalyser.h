565820a8917027f0c9d4f5c73fe4290f5eac848d
FELLAH CHOUAIB <chouaib.fellah@etu.unistra.fr>
2018-12-09 02:58:20 +0100

    find_first_slot instead find_slot

--- include/DigitalDifferentialAnalyser.h
+++ include/DigitalDifferentialAnalyser.h
@@ -10,7 +10,7 @@ private :
 
 public :
     vector<Slot> find_visited_grids(Ray ray_normalized, Grid grid);
-    Slot find_slot(Grid grid, Ray ray_normalized);
+    Slot find_first_slot(Grid grid, Ray ray_normalized);
     std::vector<Slot*> Slots(Ray r, Grid grid);
     vector<Slot*> Slots_visited(Ray r, Grid grid);
 
