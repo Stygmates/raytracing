94dafffcfce2c94f102ac50c2d060651bef07982
stygmates <styg.tan.dat@gmail.com>
2018-12-09 20:19:53 +0100

    Replaced unnecessary import

--- src/Loader.cpp
+++ src/Loader.cpp
@@ -1,12 +1,11 @@
 
 #include <string>
 #include <vector>
-#include "../include/Loader.h"
-#include <vector>
 #include <cstdio>
 #include <iostream>
 #include <cassert>
 #include <fstream>
+#include "../include/Loader.h"
 
 Loader::Loader()
 {
@@ -22,7 +21,7 @@ void Loader::import( std::string filename )
 
 void Loader::loadData(std::vector<Triangle> &triangles)
 {
-    vector<glm::vec3> positions;
+    vector<Point> positions;
     if( this->scene->HasMeshes() )
     {
         for( unsigned int i = 0; i < this->scene->mNumMeshes; i++ )
@@ -31,7 +30,7 @@ void Loader::loadData(std::vector<Triangle> &triangles)
             for( unsigned int j = 0; j < mesh->mNumVertices; j++ )
             {
                 const aiVector3D& position = mesh->mVertices[ j ];
-                glm::vec3 v = glm::vec3( position.x, position.y, position.z );
+                Point v = Point( position.x, position.y, position.z );
                 positions.push_back( v );
             }
             for( unsigned int j = 0; j < mesh->mNumFaces; j++ )
@@ -39,9 +38,9 @@ void Loader::loadData(std::vector<Triangle> &triangles)
                 const aiFace& face = mesh->mFaces[ j ];
                 if( face.mNumIndices == 3 )
                 {
-                    Point p1(positions[face.mIndices[0]].x, positions[face.mIndices[0]].y, positions[face.mIndices[0]].z);
-                    Point p2(positions[face.mIndices[1]].x, positions[face.mIndices[1]].y, positions[face.mIndices[1]].z);
-                    Point p3(positions[face.mIndices[2]].x, positions[face.mIndices[2]].y, positions[face.mIndices[2]].z);
+                    Point p1(positions[face.mIndices[0]].get_x(), positions[face.mIndices[0]].get_y(), positions[face.mIndices[0]].get_z());
+                    Point p2(positions[face.mIndices[1]].get_x(), positions[face.mIndices[1]].get_y(), positions[face.mIndices[1]].get_z());
+                    Point p3(positions[face.mIndices[2]].get_x(), positions[face.mIndices[2]].get_y(), positions[face.mIndices[2]].get_z());
                     Triangle t(p1, p2, p3);
                     triangles.push_back(t);
                 }
